6e9c0eaece78be848983ec05ff03c66ab98f7319
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-04-17 23:39:13 +0900

    update

--- soruto-spliter.js
+++ soruto-spliter.js
@@ -5,6 +5,12 @@ var sjws = new Object;
 //Main
 sjws.split = function(st){
 var stl = st.toLowerCase();
+if(stl.match(/をけんさくして/)){stl=stl.slice(0,-7);}
+if(stl.match(/で検索して|を検索して|をけんさく/)){stl=stl.slice(0,-5);}
+if(stl.match(/検索して|を教えて|を調べて/)){stl=stl.slice(0,-4);}
+if(stl.match(/で検索|を検索|教えて|調べて/)){stl=stl.slice(0,-3);}
+if(stl.match(/ひらいて|を開いて/)){stl=stl.slice(0,-4);}
+if(stl.match(/開いて/)){stl=stl.slice(0,-3);}
 var name = new Array(1);
 var index = stl.indexOf("で");
 var index2 = stl.indexOf(":");
