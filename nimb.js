5f5218f3280364f31bd3d04621a3d2c6cfefa3f2
SorutoProject <32834893+SorutoProject@users.noreply.github.com>
2018-04-17 23:09:28 +0900

    Upload Beta Version

---
+++ nimb.js
@@ -0,0 +1,61 @@
+﻿window.onload = function(){
+	document.getElementById("ut").focus();
+}
+function sMenu(){
+	document.getElementById("menu").style.display = "block";
+}
+function cMenu(){
+	document.getElementById("menu").style.display = "none";
+}
+function reply(){
+	var ut = document.getElementById("ut").value;//ユーザーの発言を取得
+	var utencode = encodeURI(ut);//utをURLエンコード
+	var mencode = utencode.toLowerCase();
+	var encode = mencode.replace(/%/g , "");
+	var ans = document.getElementById("ans");//Nimb 返答エリアのエレメントを取得
+	var datas = sjws.split(ut);//Soruto Spliterを使って、サービス名、検索ワードを取得
+	//nimb データ取得開始
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET',"txtdb/" + mencode + ".txt", true);
+	xhr.onreadystatechange = function(){
+    if (xhr.readyState === 4 && xhr.status === 200){
+      analysis(xhr.responseText,datas,ans);
+    }else{
+		ans.innerHTML = '<span style="color:orange">データの取得に失敗しました</span>';
+	}
+  };
+  xhr.send(null);
+}
+function analysis(nimbdata,datas,ans){
+	var nimbsplit = nimbdata.split("|");//nimb データを配列に分割する
+	var length = nimbsplit.length;
+	/*
+	nimb データ配列
+	URLのみの形式: [0]:nimbの発言 [1]:開くURL 総配列数:1
+	検索エンジン込み形式: [0]:検索ワードがあるときの発言(*を検索キーワードに置き換え) [1]:←のときに使う検索URL [2]:検索ワードがカラのときの発言 [3]:←のときに開くURL 総配列数:3
+	*/
+	
+	//nimbデータがURL形式のとき
+	if(length == 1){
+		ans.innerHTML = nimbdata[0];
+		window.setTimeout("MovePage('" + url +"')",500);
+	}
+	//nimbデータが検索エンジン込み形式のとき
+	else if(length == 3){
+		//ユーザーの検索キーワードがあるとき
+		if(datas[1]!=false){
+			ans.innerHTML = nimbdata[0].split("*").join('"' + datas[1] + '"');
+			var swurl = encodeURI(nimbdata[1] + datas[1]);
+			window.setTimeout("MovePage('" + swurl +"')",500);
+		}
+		//検索キーワードがないとき
+		else{
+			ans.innerHTML = nimbdata[2];
+			window.setTimeout("MovePage('" + nimbdata[3] +"')",500);
+		}
+	}
+	
+}
+function MovePage(url){
+	location.href=url;
+}
---
+++ soruto-spliter.js
@@ -0,0 +1,41 @@
+﻿/*Soruto Japanese word spliter
+*(c)2018 Soruto Project.
+*/
+var sjws = new Object;
+//Main
+sjws.split = function(st){
+var stl = st.toLowerCase();
+var name = new Array(1);
+var index = stl.indexOf("で");
+var index2 = stl.indexOf(":");
+var index3 = stl.indexOf("を");
+//サービス名が文頭にあるとき(~で~)
+if(index !=-1){
+	name[0]=stl.slice(0,index);
+	name[1] = stl.slice(index + 1);
+	}
+//:が含まれるとき(http://~)
+else if(index2 !=-1){
+	name[0]=stl.slice(0,index2);
+	name[1] = stl.slice(index2 + 1);
+}
+//検索ワードが文頭にあるとき(~を~)
+else if(index3 != -1){
+	name[0] = stl.slice(index3+1);
+	name[1] = stl.slice(0,index3);
+	}
+else{
+name[0] = stl;
+name[1] = false;
+}
+//サービス名の空白を消す
+var targetStr = "　" ;
+var regExp = new RegExp( targetStr, "g" ) ;
+name[0] = name[0].replace( regExp , "" ) ;
+
+var targetStr = " " ;
+var regExp = new RegExp( targetStr, "g" ) ;
+name[0] = name[0].replace( regExp , "" ) ;
+
+return name;
+}
